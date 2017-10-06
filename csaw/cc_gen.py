6279363b3a436cd8c625bbf6f83fbfad0e9d79cc
kds <kds@mitre.org>
2017-10-06 10:37:11 -0400

    Added checkdigit func to verify cards

--- csaw/cc_gen.py
+++ csaw/cc_gen.py
@@ -45,6 +45,31 @@ jcbPrefixList = [['3', '5']]
 
 voyagerPrefixList = [['8', '6', '9', '9']]
 
+def get_checkdigit(ccnumber, length):
+    sum = 0
+    pos = 0
+
+    reversedCCnumber = []
+    reversedCCnumber.extend(ccnumber)
+    reversedCCnumber.reverse()
+
+    while pos < length - 1:
+
+        odd = int(reversedCCnumber[pos]) * 2
+        if odd > 9:
+            odd -= 9
+
+        sum += odd
+
+        if pos != (length - 2):
+            sum += int(reversedCCnumber[pos + 1])
+
+        pos += 2
+
+    # Calculate check digit
+
+    checkdigit = ((sum / 10 + 1) * 10 - sum) % 10
+    return checkdigit
 
 def completed_number(prefix, suffix, length):
     """
@@ -80,30 +105,8 @@ def completed_number(prefix, suffix, length):
         ccnumber.extend(suffix_str[:len(suffix)-1])
         # Calculate sum
 
-        sum = 0
-        pos = 0
+        checkdigit = get_checkdigit(ccnumber, length)
 
-        reversedCCnumber = []
-        reversedCCnumber.extend(ccnumber)
-        reversedCCnumber.reverse()
-
-        while pos < length - 1:
-
-            odd = int(reversedCCnumber[pos]) * 2
-            if odd > 9:
-                odd -= 9
-
-            sum += odd
-
-            if pos != (length - 2):
-
-                sum += int(reversedCCnumber[pos + 1])
-
-            pos += 2
-
-        # Calculate check digit
-
-        checkdigit = ((sum / 10 + 1) * 10 - sum) % 10
         if not suffix:
             desired_checkdigit = checkdigit
 
